017b7a1e1915f020d5eba683df253b5ddebe0600
Daniel <dani.v2509@outlook.com>
2024-10-23 13:41:57 +0200

    Solution

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,33 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        BufferedReader br = null; // Inicializamos el buffered reader vacío
+
+        /*
+             El Buffer Reader almacena los datos en buffer para ahorrar una cantidad
+             considerable de recursos.
+
+             File reader es un método que lee secuencias de carácteres.
+        */
+
+        try {
+            // Probamos a inicializar el Buffered reader
+            br = new BufferedReader(new FileReader("src/main/resources/lorem.txt"));
+
+            // Hacemos un bucle que recorra las líneas
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+
 
     }
 }
